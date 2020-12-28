4688a0255d51c0a48456dd45cf7837180a1b7404
Brian Sam-Bodden <brian.sam-bodden@redislabs.com>
2020-12-27 21:53:22 -0700

    feat: implement sample data loader using CommandLineRunner

--- src/main/java/com/redislabs/edu/todo/TodoApplication.java
+++ src/main/java/com/redislabs/edu/todo/TodoApplication.java
@@ -1,11 +1,15 @@
 package com.redislabs.edu.todo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+
+import com.redislabs.edu.todo.domain.Todo;
+import com.redislabs.edu.todo.repository.TodoRepository;
 
 @SpringBootApplication
 @EnableAutoConfiguration
@@ -15,6 +19,16 @@ public class TodoApplication {
     RedisTemplate<?, ?> template = new RedisTemplate<>();
     template.setConnectionFactory(connectionFactory);
     return template;
+  }
+  
+  @Bean
+  public CommandLineRunner loadData(TodoRepository repository) {
+    return args -> {
+      repository.deleteAll();
+      repository.save(Todo.builder().title("Wake up").build());
+      repository.save(Todo.builder().title("Fall out of bed").build());
+      repository.save(Todo.builder().title("Drag a comb across your head").build());
+    };
   }
 
   public static void main(String[] args) {
