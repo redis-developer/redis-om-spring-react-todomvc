bb5778800816b287988d9e3f924d6587b9297325
Brian Sam-Bodden <brian.sam-bodden@redislabs.com>
2020-12-27 21:46:32 -0700

    feat: add RedisTemplate Bean and Redis connection config

--- src/main/java/com/redislabs/edu/todo/TodoApplication.java
+++ src/main/java/com/redislabs/edu/todo/TodoApplication.java
@@ -1,10 +1,21 @@
 package com.redislabs.edu.todo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class TodoApplication {
+  @Bean
+  public RedisTemplate<?, ?> redisTemplate(RedisConnectionFactory connectionFactory) {
+    RedisTemplate<?, ?> template = new RedisTemplate<>();
+    template.setConnectionFactory(connectionFactory);
+    return template;
+  }
 
   public static void main(String[] args) {
     SpringApplication.run(TodoApplication.class, args);
