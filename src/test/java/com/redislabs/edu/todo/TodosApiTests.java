980632ba75b4ef9b366dab0630d36b8a14a96b85
Brian Sam-Bodden <brian.sam-bodden@redislabs.com>
2020-12-28 14:08:26 -0700

    test: test GET /todos with sample data

--- src/test/java/com/redislabs/edu/todo/TodosApiTests.java
+++ src/test/java/com/redislabs/edu/todo/TodosApiTests.java
@@ -3,6 +3,10 @@ package com.redislabs.edu.todo;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +15,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.mockito.BDDMockito.given;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.hasSize;
+
+import com.redislabs.edu.todo.domain.Todo;
 import com.redislabs.edu.todo.repository.TodoRepository;
 
 @SpringBootTest
@@ -30,4 +39,25 @@ public class TodosApiTests {
         .andExpect(content().string("[]"));
   }
 
+  @Test
+  public void testGivenTodosGetAllReturnsAJsonArrayOfTodos() throws Exception {
+
+    Todo todo1 = Todo.builder().title("Wake up").build();
+    Todo todo2 = Todo.builder().title("Fall out of bed").build();
+    Todo todo3 = Todo.builder().title("Drag a comb across your head").build();
+
+    List<Todo> allTodos = new ArrayList<Todo>();
+    allTodos.add(todo1);
+    allTodos.add(todo2);
+    allTodos.add(todo3);
+
+    given(todoRepository.findAll()).willReturn(allTodos);
+
+    mvc.perform(get("/todos")) //
+        .andExpect(status().isOk()) //
+        .andExpect(jsonPath("$", hasSize(3))) //
+        .andExpect(jsonPath("$[0].title", is(todo1.getTitle()))) //
+        .andExpect(jsonPath("$[1].title", is(todo2.getTitle()))) //
+        .andExpect(jsonPath("$[2].title", is(todo3.getTitle())));
+  }
 }
