059a481d42b8e26c8273910165e8e011c3cc7ce6
Brian Sam-Bodden <brian.sam-bodden@redislabs.com>
2020-12-28 20:34:19 -0700

    feat: implement GET /todos/{id}

--- src/main/java/com/redislabs/edu/todo/controllers/TodosController.java
+++ src/main/java/com/redislabs/edu/todo/controllers/TodosController.java
@@ -14,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -57,6 +58,11 @@ public class TodosController {
   @DeleteMapping
   public void deleteAll() {
     repository.deleteAll();
+  }
+
+  @GetMapping("/{id}")
+  public Todo get(@PathVariable("id") Long id) {
+    return repository.findById(id).get();
   }
 
 }
