fb77f4c9c999917191b8818a8a2d4bb993fadabc
Brian Sam-Bodden <brian.sam-bodden@redislabs.com>
2020-12-28 17:16:23 -0700

    feat: implement DELETE /todos

--- src/main/java/com/redislabs/edu/todo/controllers/TodosController.java
+++ src/main/java/com/redislabs/edu/todo/controllers/TodosController.java
@@ -12,6 +12,7 @@ import java.util.stream.StreamSupport;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -51,6 +52,11 @@ public class TodosController {
   @ResponseStatus(HttpStatus.CREATED)
   public Todo create(@RequestBody Todo todo) {
     return repository.save(todo);
+  }
+  
+  @DeleteMapping
+  public void deleteAll() {
+    repository.deleteAll();
   }
 
 }
