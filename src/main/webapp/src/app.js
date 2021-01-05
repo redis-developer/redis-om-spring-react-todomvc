54ec0586a01ac9a83fe474dda980004cacbd1c6a
Brian Sam-Bodden <brian.sam-bodden@redislabs.com>
2021-01-04 19:39:05 -0700

    feat(ui): add footer component

--- src/main/webapp/src/app.js
+++ src/main/webapp/src/app.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import Footer from './components/footer';
+
 ReactDOM.render(
-  <h1>Hello, world!</h1>,
+  <React.Fragment>
+    <section className="todoapp">
+      <header className="header">
+        <h1>todos</h1>
+      </header>
+    </section>
+    <Footer />
+  </React.Fragment>,
   document.getElementById('react')
 );
