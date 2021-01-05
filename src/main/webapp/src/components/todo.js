e86a35dbea00526b904d9f211ca54cc977313df9
Brian Sam-Bodden <brian.sam-bodden@redislabs.com>
2021-01-04 23:27:18 -0700

    feat(ui): implement abort edit on click outside

--- src/main/webapp/src/components/todo.js
+++ src/main/webapp/src/components/todo.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useRef, useContext } from 'react';
 import { TodosContext } from "../context/todos_context";
 import { KEY_RETURN, KEY_ESCAPE } from 'keycode-js';
+import useOnClickOutside from "../hooks/use_on_click_outside";
 
 const Todo = function (props) {
   const { getTodo, updateTodo, deleteTodo }  = useContext(TodosContext);
@@ -8,12 +9,18 @@ const Todo = function (props) {
   const [editing, setEditing] = useState(false);
   const [title, setTitle] = useState(todo.title);
   const [titleBeforeEditing, setTitleBeforeEditing] = useState("");
+  const wrapperRef = useRef(null);
 
   useEffect(() => {
     if (editing) {
       setTitleBeforeEditing(title);
     }
   }, [editing]);
+
+  // check to see if the user clicked outside of this component
+  useOnClickOutside(wrapperRef, () => {
+    if (editing) setEditing(false);
+  });
 
   const toggleTodo = () => {
     todo.completed = !todo.completed;
@@ -45,6 +52,7 @@ const Todo = function (props) {
     <li
       onDoubleClick={() => setEditing(val => !val)}
       className={`${editing ? "editing" : ""}`}
+      ref={wrapperRef}
     >
       <div className="view">
         <input
