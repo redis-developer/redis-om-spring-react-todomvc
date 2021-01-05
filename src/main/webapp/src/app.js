db0f815af7ebe6fc699b96fec6b4e05039b2e708
Brian Sam-Bodden <brian.sam-bodden@redislabs.com>
2021-01-05 00:16:31 -0700

    feat(ui): add React Router HashRouter around TodoList component

--- src/main/webapp/src/app.js
+++ src/main/webapp/src/app.js
@@ -6,6 +6,7 @@ import TodoList from './components/todo_list';
 import Footer from './components/footer';
 
 import { TodosContextProvider } from "./context/todos_context";
+import { HashRouter as Router, Route } from "react-router-dom";
 
 ReactDOM.render(
   <TodosContextProvider>
@@ -14,7 +15,9 @@ ReactDOM.render(
         <h1>todos</h1>
         <TodoForm />
       </header>
-      <TodoList />
+      <Router>
+        <Route path="/:filter?" component={TodoList} />
+      </Router>
     </section>
     <Footer />
   </TodosContextProvider>,
