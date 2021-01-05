064754f03d4aa9df335dab7b4485f92dfb0bab42
Brian Sam-Bodden <brian.sam-bodden@redislabs.com>
2021-01-04 19:39:05 -0700

    feat(ui): add footer component

---
+++ src/main/webapp/src/components/footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = function () {
+  return (
+    <footer className="info">
+      <p>Double-click to edit a todo</p>
+      <p>Created by <a href="http://github.com/bsbodden/">bsbodden</a></p>
+      <p>Part of <a href="http://todomvc.com">TodoMVC</a></p>
+    </footer>
+  );
+}
+
+export default Footer;
