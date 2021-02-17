f1a98faca2344f8fcd98fd53bb0a3adaae2cbb94
Brian Sam-Bodden <brian.sam-bodden@redislabs.com>
2021-02-17 11:41:37 -0700

    test(ui): introduce page model and test footer component

--- src/main/webapp/src/app.test.js
+++ src/main/webapp/src/app.test.js
@@ -1,11 +1,24 @@
 import { Selector } from 'testcafe';
 import { ClientFunction } from 'testcafe';
 
+class TodosPage {
+  constructor() {
+    this.title = Selector("#react > section > header > h1");
+    this.footerInfo = Selector("#react > footer");
+  }
+}
+
 const baseUrl = 'http://localhost:8080/';
+const page = new TodosPage();
 
 fixture('The home page').page(baseUrl);
 
-test('should show Hello World', async t => {
+test('should show the todos title', async t => {
   await t
-    .expect(Selector("#react > h1").innerText).contains("Hello, world!");
+    .expect(page.title.innerText).contains("todos");
+});
+
+test('should show the footer info', async t => {
+  await t
+    .expect(page.footerInfo.visible).ok();
 });
