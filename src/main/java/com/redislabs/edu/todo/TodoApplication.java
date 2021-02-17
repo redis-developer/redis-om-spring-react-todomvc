028a12c0dd00a52b16fcfce33280eee0f53eb928
Brian Sam-Bodden <brian.sam-bodden@redislabs.com>
2021-02-17 14:13:58 -0700

    test(ui): add test scenario for 'No Todos'

--- src/main/java/com/redislabs/edu/todo/TodoApplication.java
+++ src/main/java/com/redislabs/edu/todo/TodoApplication.java
@@ -20,14 +20,14 @@ public class TodoApplication {
     template.setConnectionFactory(connectionFactory);
     return template;
   }
-  
+
   @Bean
   public CommandLineRunner loadData(TodoRepository repository) {
     return args -> {
       repository.deleteAll();
-      repository.save(Todo.builder().title("Wake up").build());
-      repository.save(Todo.builder().title("Fall out of bed").build());
-      repository.save(Todo.builder().title("Drag a comb across your head").build());
+      // repository.save(Todo.builder().title("Wake up").build());
+      // repository.save(Todo.builder().title("Fall out of bed").build());
+      // repository.save(Todo.builder().title("Drag a comb across your head").build());
     };
   }
 
--- src/main/webapp/src/app.test.js
+++ src/main/webapp/src/app.test.js
@@ -4,7 +4,18 @@ import { ClientFunction } from 'testcafe';
 class TodosPage {
   constructor() {
     this.title = Selector("#react > section > header > h1");
+    this.footer = Selector("footer.footer");
     this.footerInfo = Selector("#react > footer");
+    this.toogleAllButton = Selector("label[for=toggle-all]");
+    this.clearCompletedButton = Selector("button.clear-completed");
+
+    this.itemCount = () => {
+      return Selector("ul.todo-list > li").count
+    }
+
+    this.completedItemCount = () => {
+      return Selector("ul.todo-list > li input.toggle:checked").count
+    }
   }
 }
 
@@ -21,4 +32,23 @@ test('should show the todos title', async t => {
 test('should show the footer info', async t => {
   await t
     .expect(page.footerInfo.visible).ok();
+});
+
+fixture('No Todos')
+  .page(baseUrl)
+  .beforeEach(async t => {
+    const itemCount = await page.itemCount();
+    const completedItemCount = await page.completedItemCount();
+    if (itemCount > 0) {
+      if (completedItemCount < itemCount) {
+        await t.click(page.toogleAllButton)
+      }
+      await t.click(page.clearCompletedButton);
+    }
+  });
+
+test('should hide #main and #footer', async t => {
+  await t
+    .expect(page.itemCount()).eql(0)
+    .expect(page.footer.visible).notOk();
 });
