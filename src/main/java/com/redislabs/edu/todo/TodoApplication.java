8587ea7fe4f0967190f50ae6b6244c278ca4c7de
Brian Sam-Bodden <brian.sam-bodden@redislabs.com>
2022-04-18 23:00:41 -0700

    feat: add swagger UI

--- src/main/java/com/redislabs/edu/todo/TodoApplication.java
+++ src/main/java/com/redislabs/edu/todo/TodoApplication.java
@@ -5,11 +5,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 import com.redis.om.spring.annotations.EnableRedisDocumentRepositories;
 import com.redislabs.edu.todo.repository.TodoRepository;
 
 @SpringBootApplication
 @EnableRedisDocumentRepositories
+@EnableSwagger2
 public class TodoApplication {
 
   @Bean
@@ -20,6 +27,15 @@ public class TodoApplication {
       // repository.save(Todo.builder().title("Fall out of bed").build());
       // repository.save(Todo.builder().title("Drag a comb across your head").build());
     };
+  }
+
+  @Bean
+  public Docket api() {
+    return new Docket(DocumentationType.SWAGGER_2)
+        .select()
+        .apis(RequestHandlerSelectors.any())
+        .paths(PathSelectors.any())
+        .build();
   }
 
   public static void main(String[] args) {
